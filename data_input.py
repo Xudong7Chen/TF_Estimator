a2aa8a7434d65d941ebd2d3da3b4286c0d954828
Xudong7Chen <Xudong7Chen@gmail.com>
2018-08-19 21:36:44 +0800

    estimator

--- data_input.py
+++ data_input.py
@@ -6,7 +6,8 @@ def get_data(name, batch_size=1, data_shape=16, train=True):
         parse_dict = {'data':tf.FixedLenFeature(shape=(16) , dtype=tf.int64, default_value=None),\
                       'label':tf.FixedLenFeature(shape=(1), dtype=tf.int64, default_value=None)}
         parse_example = tf.parse_single_example(example_proto, parse_dict)
-        parse_example['label'] = tf.one_hot(parse_example['label'], 2)
+        parse_example['label'] = tf.one_hot(parse_example['label'], 2, 1, 0)
+        parse_example['label'] = tf.reshape(parse_example['label'], (2,))
         return parse_example
 
     with tf.name_scope('input_pipeline'):
@@ -14,6 +15,7 @@ def get_data(name, batch_size=1, data_shape=16, train=True):
         dataset = dataset.map(parse_function)
         dataset = dataset.shuffle(buffer_size=1500)
         dataset = dataset.batch(batch_size)
+        dataset = dataset.repeat()
         iterator = dataset.make_one_shot_iterator()
         next_element = iterator.get_next()
     return next_element
--- main.py
+++ main.py
@@ -6,89 +6,64 @@ import os
 from config import get_config
 from data_input import get_data 
 
+tf.logging.set_verbosity(tf.logging.INFO)
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 para, _ = get_config()
 
+def my_model(features, labels, mode, params):
 
-print('Creating input pipeline...') #数据以#号结束
+    def prelu(inputs):
+        alphas = tf.get_variable("alphas", shape=inputs.get_shape()[-1], dtype=tf.float32, initializer=tf.constant_initializer(0.25))
+        pos = tf.nn.relu(inputs)
+        neg = alphas * (inputs-abs(inputs))*0.5
+        return pos + neg
+        
+    labels = features['label']
+    inputs = tf.feature_column.input_layer(features, params['feature_columns'])
+    net = tf.layers.dense(inputs, 128, activation=prelu, name='dense_0')    
+    net = tf.layers.dense(net, 64, activation=prelu, name='dense_1')
+    net = tf.layers.dense(net, 32, activation=prelu, name='dense_2')
+    logits = tf.layers.dense(net, params["num_of_class"], activation=None, name='dense_3')
+    predicted_class = tf.argmax(logits, 1)
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        predictions = {
+            'class_ids': predicted_class,
+            'probabilities': tf.nn.softmax(logits),
+            'logits': logits
+        }
+        return tf.estimator.EstimatorSpec(mode, predictions)
+
+    loss = tf.losses.softmax_cross_entropy(labels, logits) 
+    accuracy = tf.metrics.accuracy(labels=labels, predictions=predicted_class)
+    metrics = {'accuracy': accuracy}
+    tf.summary.scalar('accuracy', accuracy[1])
+
+    if mode == tf.estimator.ModeKeys.EVAL:
+        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
+        train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+    
+
+
 train = get_data(para.file_train, para.batch_size)
 valid = get_data(para.file_valid, para.batch_size)
-print('Success.')    
+my_feature_columns = [tf.feature_column.numeric_column(key='data')]
 
-train_data_num = para.train_data_num
-valid_data_num = para.valid_data_num
+classifier = tf.estimator.Estimator(
+    model_fn=my_model,
+    params={
+        'feature_columns': my_feature_columns,
+        'num_of_class': 2
+        })
 
-total_steps = int(para.total_epochs * train_data_num / para.batch_size)
-epoch_learning_rate = para.ini_learning_rate
+classifier.train(
+    input_fn=lambda:get_data(para.file_train, para.batch_size),
+    steps=100000)
 
-# creat graph---------------------------------------------------------------------------------------------------------------------------------------
-inputs_placeholder = tf.placeholder(tf.float32, shape=[None, para.data_shape])
-label_placeholder = tf.placeholder(tf.float32, shape=[None, 2])
-global_step = tf.Variable(0, trainable=False, name='global_step')
-
-loss, network_output, loss_visible = gen_graph(inputs_placeholder, label_placeholder, 2)
-optimizer = tf.train.AdamOptimizer(epoch_learning_rate)
-train_op = optimizer.minimize(loss, global_step)
-saver = tf.train.Saver(max_to_keep=4)
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
-epoch = 0
-sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-# sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-# sess = tf_debug.TensorBoardDebugWrapperSession(sess, 'localhost:6064')
-
-#visualize some variables
-tf.summary.scalar("cls_loss", loss)#cls_loss
-summary_op = tf.summary.merge_all()
-
-writer = tf.summary.FileWriter('./logs', sess.graph)
-
-coord = tf.train.Coordinator()
-threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-
-if para.is_train == True:
-    sess.run(tf.global_variables_initializer())
-    for step in range(1, total_steps):
-
-        if (step * para.batch_size) % train_data_num == 0:
-            epoch = epoch + 1
-        training_array, training_label = sess.run([train['data'], train['label']])
-        train_feed_dict = {
-            inputs_placeholder: training_array,
-            label_placeholder: training_label}
-
-        batch_loss, batch_output, _, summary = sess.run([loss, network_output, train_op, summary_op], feed_dict=train_feed_dict)
-
-        if epoch == (para.total_epochs * 0.5) or epoch == (para.total_epochs * 0.7):
-            epoch_learning_rate = epoch_learning_rate / 10
-
-        if step % 2000 == 0:
-            print('Validing----------------------------------')
-            for _ in range(5):
-                validing_array, validing_label = sess.run([valid['data'], valid['label']])
-                valid_feed_dict = {
-                    inputs_placeholder: validing_array,
-                    label_placeholder: validing_label}
-                valid_batch_loss, valid_batch_output = sess.run([loss, network_output], feed_dict=valid_feed_dict)
-                test_accuracy = cal_acc(validing_label, valid_batch_output, para.batch_size)
-                print('batch_accuracy: %f' % test_accuracy)
-                print('batch_loss: %f' % valid_batch_loss)
-            print('Finish Validing---------------------------')
-
-        if step % 20 == 0:
-            batch_accuracy = cal_acc(training_label, batch_output, para.batch_size)
-            print('steps: %d / %d' % (step, total_steps))
-            print('accuracy: %f' % batch_accuracy)
-            print('loss: %f' % batch_loss)
-            print('cur_lr: %f' % epoch_learning_rate)
-            print('------------------------------------------')
-            
-        if step % 2000 == 0:
-            #store model
-            print('storing model')
-            saver.save(sess=sess, save_path='./models/zzc_%d' % step)
-        
-        writer.add_summary(summary,global_step=step)
-
-coord.request_stop()
-coord.join(threads)
-sess.close()
+eval_result = classifier.evaluate(
+    input_fn=lambda:get_data(para.file_valid, para.batch_size))
+print(eval_result)
