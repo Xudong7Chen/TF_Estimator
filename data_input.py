aa86f4df3a328e757d5f844efc20cd19dc5f6ca1
XudongChen <xudong7chen@gmail.com>
2018-08-24 19:17:26 +0800

    estimator

--- data_input.py
+++ data_input.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 
 def get_data(name, batch_size=1, data_shape=16, train=True):
     def parse_function(example_proto):
-        parse_dict = {'data':tf.FixedLenFeature(shape=(16) , dtype=tf.int64, default_value=None),\
-                      'label':tf.FixedLenFeature(shape=(1), dtype=tf.int64, default_value=None)}
+        parse_dict = {'data':tf.FixedLenFeature(shape=(16,) , dtype=tf.int64, default_value=None),\
+                      'label':tf.FixedLenFeature(shape=(1,), dtype=tf.int64, default_value=None)}
         parse_example = tf.parse_single_example(example_proto, parse_dict)
         parse_example['label'] = tf.one_hot(parse_example['label'], 2, 1, 0)
         parse_example['label'] = tf.reshape(parse_example['label'], (2,))
@@ -18,7 +18,7 @@ def get_data(name, batch_size=1, data_shape=16, train=True):
         dataset = dataset.repeat()
         iterator = dataset.make_one_shot_iterator()
         next_element = iterator.get_next()
-    return next_element
+    return next_element, next_element.pop('label')
 
 # next_element = get_data('./data/tfrecord/data_train.tfrecord')
 # sess = tf.InteractiveSession()
--- main.py
+++ main.py
@@ -6,64 +6,55 @@ import os
 from config import get_config
 from data_input import get_data 
 
-tf.logging.set_verbosity(tf.logging.INFO)
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 para, _ = get_config()
 
-def my_model(features, labels, mode, params):
+
+def my_model_fn(features, labels, mode, params):
 
     def prelu(inputs):
         alphas = tf.get_variable("alphas", shape=inputs.get_shape()[-1], dtype=tf.float32, initializer=tf.constant_initializer(0.25))
         pos = tf.nn.relu(inputs)
         neg = alphas * (inputs-abs(inputs))*0.5
         return pos + neg
-        
-    labels = features['label']
+
     inputs = tf.feature_column.input_layer(features, params['feature_columns'])
-    net = tf.layers.dense(inputs, 128, activation=prelu, name='dense_0')    
-    net = tf.layers.dense(net, 64, activation=prelu, name='dense_1')
-    net = tf.layers.dense(net, 32, activation=prelu, name='dense_2')
-    logits = tf.layers.dense(net, params["num_of_class"], activation=None, name='dense_3')
-    predicted_class = tf.argmax(logits, 1)
+    with tf.name_scope('Network'):
+        net = tf.layers.dense(inputs, 128, activation=prelu, name='dense_0')    
+        net = tf.layers.dense(net, 64, activation=prelu, name='dense_1')
+        net = tf.layers.dense(net, 32, activation=prelu, name='dense_2')
+        logits = tf.layers.dense(net, params['num_of_classes'], activation=None, name='dense_3')
+        predicted_classes = tf.argmax(logits, 1)
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         predictions = {
-            'class_ids': predicted_class,
+            'class_ids': predicted_classes[:, tf.newaxis],
             'probabilities': tf.nn.softmax(logits),
-            'logits': logits
-        }
-        return tf.estimator.EstimatorSpec(mode, predictions)
-
+            'logits': logits}
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+    
     loss = tf.losses.softmax_cross_entropy(labels, logits) 
-    accuracy = tf.metrics.accuracy(labels=labels, predictions=predicted_class)
-    metrics = {'accuracy': accuracy}
-    tf.summary.scalar('accuracy', accuracy[1])
-
-    if mode == tf.estimator.ModeKeys.EVAL:
-        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
-
+    
     if mode == tf.estimator.ModeKeys.TRAIN:
         optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
         train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
         return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
     
+    accuracy = tf.metrics.accuracy(labels=labels,
+                                   predictions=logits)
+    metrics = {'accuracy': accuracy}
+    tf.summary.scalar('accuracy', accuracy)
+
+    if mode == tf.estimator.ModeKeys.EVAL:
+        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
 
 
-train = get_data(para.file_train, para.batch_size)
-valid = get_data(para.file_valid, para.batch_size)
-my_feature_columns = [tf.feature_column.numeric_column(key='data')]
-
-classifier = tf.estimator.Estimator(
-    model_fn=my_model,
-    params={
-        'feature_columns': my_feature_columns,
-        'num_of_class': 2
-        })
-
-classifier.train(
-    input_fn=lambda:get_data(para.file_train, para.batch_size),
-    steps=100000)
-
-eval_result = classifier.evaluate(
-    input_fn=lambda:get_data(para.file_valid, para.batch_size))
-print(eval_result)
+tf.logging.set_verbosity(tf.logging.INFO)
+feature_columns = [tf.feature_column.numeric_column(key='data', shape=(16,))]
+classifier = tf.estimator.Estimator(model_fn=my_model_fn,
+                                    model_dir='./models',
+                                    params={'feature_columns': feature_columns,
+                                            'num_of_classes': 2})
+a = classifier.predict(input_fn=lambda:get_data(para.file_train, para.batch_size))
+for i in a:
+    print(i)
