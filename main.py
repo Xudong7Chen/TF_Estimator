e89bf1ed878fc8c940f4442cd9fea27a946e6321
Xudong7Chen <Xudong7Chen@gmail.com>
2018-08-25 20:57:08 +0800

    new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from network import gen_graph
+from network import my_model_fn
 from utils import cal_acc
 import os 
 from config import get_config
@@ -9,52 +9,12 @@ from data_input import get_data
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 para, _ = get_config()
 
-
-def my_model_fn(features, labels, mode, params):
-
-    def prelu(inputs):
-        alphas = tf.get_variable("alphas", shape=inputs.get_shape()[-1], dtype=tf.float32, initializer=tf.constant_initializer(0.25))
-        pos = tf.nn.relu(inputs)
-        neg = alphas * (inputs-abs(inputs))*0.5
-        return pos + neg
-
-    inputs = tf.feature_column.input_layer(features, params['feature_columns'])
-    with tf.name_scope('Network'):
-        net = tf.layers.dense(inputs, 128, activation=prelu, name='dense_0')    
-        net = tf.layers.dense(net, 64, activation=prelu, name='dense_1')
-        net = tf.layers.dense(net, 32, activation=prelu, name='dense_2')
-        logits = tf.layers.dense(net, params['num_of_classes'], activation=None, name='dense_3')
-        predicted_classes = tf.argmax(logits, 1)
-
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        predictions = {
-            'class_ids': predicted_classes[:, tf.newaxis],
-            'probabilities': tf.nn.softmax(logits),
-            'logits': logits}
-        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
-    
-    loss = tf.losses.softmax_cross_entropy(labels, logits) 
-    
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
-        train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
-    
-    accuracy = tf.metrics.accuracy(labels=labels,
-                                   predictions=logits)
-    metrics = {'accuracy': accuracy}
-    tf.summary.scalar('accuracy', accuracy)
-
-    if mode == tf.estimator.ModeKeys.EVAL:
-        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
-
-
 tf.logging.set_verbosity(tf.logging.INFO)
 feature_columns = [tf.feature_column.numeric_column(key='data', shape=(16,))]
 classifier = tf.estimator.Estimator(model_fn=my_model_fn,
                                     model_dir='./models',
                                     params={'feature_columns': feature_columns,
                                             'num_of_classes': 2})
-a = classifier.predict(input_fn=lambda:get_data(para.file_train, para.batch_size))
-for i in a:
-    print(i)
+# a = classifier.train(input_fn=lambda:get_data(para.file_train, para.batch_size), steps=100000)
+a = classifier.evaluate(input_fn=lambda:get_data(para.file_valid, para.batch_size))
+a = classifier.evaluate(input_fn=lambda:get_data(para.file_valid, para.batch_size))
--- network.py
+++ network.py
@@ -1,19 +1,45 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
+
 def prelu(inputs):
     alphas = tf.get_variable("alphas", shape=inputs.get_shape()[-1], dtype=tf.float32, initializer=tf.constant_initializer(0.25))
     pos = tf.nn.relu(inputs)
     neg = alphas * (inputs-abs(inputs))*0.5
     return pos + neg
 
-def gen_graph(inputs, label_batch, num_of_class):
+def Network(inputs, params):
     with tf.name_scope('Network'):
         net = tf.layers.dense(inputs, 128, activation=prelu, name='dense_0')    
         net = tf.layers.dense(net, 64, activation=prelu, name='dense_1')
         net = tf.layers.dense(net, 32, activation=prelu, name='dense_2')
-        net = tf.layers.dense(net, num_of_class, activation=None, name='dense_3')
-        
-        loss_visible = tf.nn.softmax(net, name='softmax_loss_visible')
-        loss = tf.losses.softmax_cross_entropy(label_batch, net) 
-        return loss, net, loss_visible
+        logits = tf.layers.dense(net, params['num_of_classes'], activation=None, name='dense_3')
+    return logits
+
+def my_model_fn(features, labels, mode, params):
+    inputs = tf.feature_column.input_layer(features, params['feature_columns'])
+    logits = Network(inputs, params)
+    predicted_classes = tf.argmax(logits, 1)
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        predictions = {
+            'class_ids': predicted_classes[:, tf.newaxis],
+            'probabilities': tf.nn.softmax(logits),
+            'logits': logits}
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+    loss = tf.losses.softmax_cross_entropy(labels, logits) 
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+
+    result = tf.one_hot(predicted_classes, 2, 1, 0)
+    accuracy = tf.metrics.accuracy(labels=labels,
+                                   predictions=result)
+    metrics = {'accuracy': accuracy}
+    tf.summary.scalar('accuracy', accuracy)
+
+    if mode == tf.estimator.ModeKeys.EVAL:
+        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
